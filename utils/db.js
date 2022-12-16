fb58a43b64a432fd52aaad0a410943721ddc9b47
Fox24 <73237980+duy2404@users.noreply.github.com>
2022-12-16 23:07:45 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -5,10 +5,10 @@ dotenv.config()
 export default knexObj({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: 'javachatapplication.mysql.database.azure.com',
+    port: '3306',
+    user: 'java',
+    password: '20Ktpm2022',
+    database: 'ptudw_finalproject_onlineacademy'
   }
 });
