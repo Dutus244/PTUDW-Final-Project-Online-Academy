0972885810656189e455feaf449baad26c043280
Fox24 <73237980+duy2404@users.noreply.github.com>
2022-12-18 22:35:04 +0700

    Update category in Admin

--- services/category.service.js
+++ services/category.service.js
@@ -11,16 +11,47 @@ export default {
         return list;
     },
 
-    add(entity) {
-        return db('categories').insert(entity);
-    },
-
     async findById(id) {
-        const list = await db('categories').where('catid', id);
+        const list = await db
+            .select('categories.catid','catname','catlevel')
+            .count({amount: 'courseid'})
+            .from('categories')
+            .leftJoin('courses', 'courses.catid', 'categories.catid')
+            .groupBy('categories.catid')
+            .where('categories.catid', id);
         if (list.length === 0) {
             return null;
         }
         return list[0];
+    },
+
+    async findAllCoursesByCatID(id) {
+        const list = await db
+            .select('courseid','coursename','categories.catname','tinydes','fulldes','rating','reviews','students','tuition','discount','discountinfo','updatetime','lecturers.lecname')
+            .from('courses')
+            .leftJoin('categories','categories.catid','courses.catid')
+            .leftJoin('lecturers','courses.lecid','lecturers.lecid')
+            .where('categories.catid', id);
+        return list;
+    },
+
+    async countCoursesByCatId(id) {
+        const list = await db
+            .count({amount: 'courseid'})
+            .from('categories')
+            .leftJoin('courses', 'courses.catid', 'categories.catid')
+            .groupBy('categories.catid')
+            .where('categories.catid', id);
+        return list[0].amount;
+    },
+
+    add(entity) {
+        return db('categories').insert(entity);
+    },
+
+    patch(entity) {
+        delete entity["amount"];
+        return db('categories').where('catid', entity.catid).update(entity);
     },
 
     del(id) {
