a02dfdc5a7bcdcfdc49c862f477c0307117c8629
Samuel <20127610@student.hcmus.edu.vn>
2022-12-23 23:12:05 +0700

    fix update profile and change password

--- routes/student.route.js
+++ routes/student.route.js
@@ -14,42 +14,51 @@ router.get('/profile', async (req, res) => {
   res.render('vwStudent/profile', {
     name: student.studentname,
     email: student.email,
-    msg: req.session.msg,
   })
-  delete req.session.msg
 })
 
 router.post('/profile', async (req, res) => {
-  await studentService.updateStudentProfile(res.locals.authUser.accountid, req.body.name, req.body.email)
-  req.session.msg = 'Cập nhật thành công'
-  res.redirect('/student/profile')
+  const student = await studentService.updateStudentProfile(res.locals.authUser.accountid, req.body.name, req.body.email)
+  if(req.body.name)
+    res.locals.authUser['name'] = req.body.name
+  if(req.body.email)
+    res.locals.authUser['email'] = req.body.email
+
+  res.render('vwStudent/profile', {
+    name: res.locals.authUser.name,
+    email: res.locals.authUser.email,
+    msg: 'Update successfully',
+  })
 })
 
 router.get('/security', async (req, res) => {
   res.render('vwStudent/security', {
     name: res.locals.authUser.name,
-    msg: req.session.msg,
-    err_msg: req.session.err_msg,
   })
-  delete req.session.msg
-  delete req.session.err_msg
 })
 
 router.post('/security', async (req, res) => {
   const user = await userService.login(req.session.authUser.email)
   const ret = bcrypt.compareSync(req.body.pass, user.pass.toString());
 
+  var msg
+  var err_msg
+
   console.log(ret);
   if (ret === false) {
-    req.session.err_msg = 'Mật khẩu hiện tại không đúng'
+    err_msg = 'Current password is incorrect'
   }
   else {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.newPass, salt);
     await userService.updatePass(req.session.authUser.accountid, hash)
-    req.session.msg = 'Đổi mật khẩu thành công'
+    msg = 'Change password successfully'
   }
-  res.redirect('/student/security')
+  res.render('vwStudent/security', {
+    name: res.locals.authUser.name,
+    msg: msg,
+    err_msg: err_msg,
+  })
 })
 
 router.get('/watchlist', async (req, res) => {
