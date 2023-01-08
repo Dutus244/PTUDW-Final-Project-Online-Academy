9d067a5788a2eacccc4b16b5c3b8d660141f8d4b
phuctran281201 <phuctran281201@gmail.com>
2023-01-08 23:57:32 +0700

    Add getTeacherCourses ver 2

--- services/lecturer.service.js
+++ services/lecturer.service.js
@@ -25,26 +25,13 @@ export default {
         return
     },
 
-    async getTeacherWatchlist(id, offset, limit) {
-        return await db
-            .select('catname', 'coursename', 'lecname', 'rating',
-                'reviews', 'tuition', 'discount', 'courses.courseid')
-            .from('watchlists')
-            .join('courses', 'watchlists.courseid', 'courses.courseid')
-            .join('categories', 'courses.catid', 'categories.catid')
-            .join('lecturers', 'courses.lecid', 'lecturers.lecid')
-            .where('watchlists.lecid', id)
-            .offset(offset)
-            .limit(limit)
-    },
-
-    async removeFromWatchlist(LecID, courseid) {
-        return await db('watchlists').where('LecID', studentid).where('courseid', courseid).del()
-    },
-
-    async addToWatchlist(LecID, courseid) {
-        return await db('watchlists')
-            .insert({'LecID': LecID, 'courseid': courseid})
+    async getTeacherCourses(id){
+        const list = await db
+            .select('courseid','coursename')
+            .from('courses')
+            .leftJoin('lecturers','courses.lecid','lecturers.lecid')
+            .where('courses.lecid', id);
+        return list;
     },
 
     async findAll() {
