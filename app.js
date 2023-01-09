405deee4e3a135e15698060078d8fc818dce25fc
Fox24 <73237980+duy2404@users.noreply.github.com>
2023-01-09 12:30:49 +0700

    update

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ import activate_routes from './middlewares/routes.mdw.js';
 import activate_error_handlers from './middlewares/error.mdw.js';
 import {fileURLToPath} from "url";
 import path, {dirname} from "path";
+import {engine} from "express-handlebars";
+import hbs_sections from "express-handlebars-sections";
+import numeral from "numeral";
+import moment from "moment/moment.js";
 
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -19,10 +23,42 @@ app.use(express.urlencoded({
 }));
 
 activate_session(app);
-activate_views(app,__dirname);
+// activate_views(app,__dirname);
 activate_resLocals(app);
 activate_routes(app);
 activate_error_handlers(app);
+
+app.engine('hbs', engine({
+  extname: 'hbs',
+  defaultLayout: 'main.hbs',
+  helpers: {
+    section: hbs_sections(),
+    format_number(val) {
+      return numeral(val).format('0,0');
+    },
+    ifEquals: function(v1, v2, options) {
+      if(v1 === v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    },
+    ifCompare: function(v1, v2, options) {
+      if(v1 > v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    },
+    format_date_ddmmyyyy(val) {
+      if (val)
+        return moment(val).format('DD-MM-YYYY')
+      else
+        return null
+    },
+  }
+}));
+console.log(__dirname)
+app.set('view engine', 'hbs');
+app.set('views', __dirname +'/views');
 
 app.listen(process.env.PORT, function () {
   console.log(`http://localhost:${process.env.PORT}`);
